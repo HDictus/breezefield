5eda2f72964dcb8477cdfdd773ef76b3b13e6374
hdictus <h.t.dictus@gmail.com>
2018-11-22 23:05:06 +0100

    allow collide to return a callback
    this can be used to call functions unavailable in box2d collision callback
    e.g. Collider.new

--- test/main.lua
+++ test/main.lua
@@ -3,14 +3,13 @@ bf = require("breezefield")
 -- TODO split into multiple test scripts
 function love.load()
    world = bf.newWorld(0, 90.81, true)
-   world.ballstospawn = 0
    ground = bf.Collider.new(world, "Polygon",
 				    {0, 550, 650, 550 , 650, 650, 0, 650})
    ground:setType("static")
 
    ball = bf.Collider.new(world, "Circle", 325, 325, 20)
       
-   ball:setRestitution(1.0)
+   ball:setRestitution(0.8)
    block1 = bf.Collider.new(world, "Polygon", {150, 375, 250, 375,
 					       250, 425, 150, 425})
    little_ball.new(
@@ -18,7 +17,10 @@ function love.load()
    
    function ball:collide(other)
       if other == block1 then
-	 world.ballstospawn = world.ballstospawn + 1
+	 -- creating Collder.new should never be called inside a callback
+	 -- a limitation of (box2d)
+	 -- instead, return a function to be called during update()
+	 return spawn_random_ball
       end
    end
 
@@ -33,12 +35,9 @@ function love.update(dt)
   elseif love.keyboard.isDown("up") then
     ball:setPosition(325, 325)
     ball:setLinearVelocity(0, 0) 
+  elseif love.keyboard.isDown("down") then
+     ball:applyForce(0, 600)
    end
-   while world.ballstospawn > 0 do
-      little_ball.new(love.math.random(love.graphics.getWidth()))
-      world.ballstospawn = world.ballstospawn - 1
-   end
-   
 end
 
 
@@ -50,19 +49,28 @@ end
 -- TODO demonstrate collision classes
 function love.mousepressed()
    local x, y
+   local radius = 30
    x, y = love.mouse.getPosition()
-   colls = world:queryCircleArea(x, y, 15)
+   colls = world:queryCircleArea(x, y, radius)
    for _, collider in ipairs(colls) do
-      if collider == ball then
-	 collider:applyLinearImpulse(0, -400)
+      if collider.identity == little_ball then
+	 local dx = love.mouse.getX() - collider:getX()
+	 local dy = love.mouse.getY() - collider:getY()
+	 local power = -5
+	 collider:applyLinearImpulse(power * dx, power * dy)
       end
    end
 end
 
 little_ball = {}
-setmetatable(little_ball, bf.Collider)
 
-function little_ball.new(...)
-   bf.Collider.new(world, 'Circle', ..., 5)
+function spawn_random_ball()
+   little_ball.new(love.math.random(love.graphics.getWidth()), 0)
+end
+
+function little_ball.new(x, y)
+   local n = bf.Collider.new(world, 'Circle', x, y, 5)
+   n.identity = little_ball
+   return n
 end
 
--- world.lua
+++ world.lua
@@ -21,10 +21,53 @@ function World:new(...)
    setmetatable(w, self)
    w._physworld = phys.newWorld(...)
    set_funcs(w, w._physworld)
+   w.update = nil -- to use our custom update
    w.colliders = {}
+   
+   -- some functions defined here to use w without being passed it
+   
+   function w.collide(obja, objb, ...)
+      -- collision event for two Colliders
+      local function run_coll(obj1, obj2, ...)
+	 if obj1.collide ~= nil then
+	    local e = obj1:collide(obj2, ...)
+	    if type(e) == 'function' then
+	       w.collide_events[#w.collide_events] = e
+	    end
+	 end
+      end
+
+      if obja ~= nil and objb ~= nil then
+	 run_coll(obja, objb, ...)
+	 run_coll(objb, obja, ...)
+      end
+   end
+
+   function w.collision(a, b, ...)
+      -- objects that hit one another can have collide methods
+      -- by default used as postSolve callback
+      local obja = a:getUserData(a)
+      local objb = b:getUserData(b)
+      w.collide(obja, objb, ...)
+   end
+
+   
+   function w.sensor_collision(a, b, ...)
+      -- because sensor collision methods aren't called otherwise
+      -- by default used as enter: callback
+      local obja = a:getUserData(a)
+      local objb = b:getUserData(b)
+      if obja:isSensor() or objb:isSensor() then
+	 w.collide(obja, objb, ...)
+      end
+   end
+
    w:setCallbacks(w.sensor_collision, nil, nil, w.collision)
+   w.collide_events = {}
+   
    return w
 end
+
 
 function World:draw(alpha, draw_over)
    -- draw the world
@@ -44,36 +87,7 @@ end
 
 -- callbacks
 
--- little utility function
-local function collide(obja, objb, ...)
-   if obja ~= nil and objb ~= nil then
-      if obja.collide ~= nil then
-	 obja:collide(objb, ...)
-      end
-      if objb.collide ~= nil then
-	 objb:collide(obja, ...)
-      end
-   end
-end
-
-function World.collision(a, b, ...)
-   -- objects that hit one another can have collide methods
-   -- by default used as postSolve callback
-   local obja = a:getUserData(a)
-   local objb = b:getUserData(b)
-   collide(obja, objb, ...)
-end
-
-
-function World.sensor_collision(a, b, ...)
-   -- because sensor collision methods aren't called otherwise
-   -- by default used as enter: callback
-   local obja = a:getUserData(a)
-   local objb = b:getUserData(b)
-   if obja:isSensor() or objb:isSensor() then
-      collide(obja, objb, ...)
-   end
-end
+-- little utility functi on
 
 -- TODO implement queryPolygonArea
 
@@ -116,6 +130,14 @@ function World:queryCircleArea(x, y, r)
       if not isin then table.remove(colls, i) end
    end
    return colls
+end
+
+function World:update(dt)
+   self._physworld:update(dt)
+   for i, v in pairs(self.collide_events) do
+      v()
+      self.collide_events[i] = nil
+   end
 end
 
 return World
